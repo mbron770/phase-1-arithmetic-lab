9ab5086ad48823c04d471a4f5e2943d0df41710a
mbron770 <mbronfin.tsd@gmail.com>
2023-05-24 20:32:19 -0400

    add arithmetic functions

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
+const num1 = 62; 
+const num2 = 1;
 
-// Write your code here
+const multiply = num1 * num2;
+
+// 2
+
+//random !== 0;  
+const random = Math.round(Math.random()*1000);
+
+//2
+
+//3 
+
+const num3 = 52; 
+const num4 = 12; 
+
+
+const mod  = num3 % num4;
+
+//3 
+
+
+//4 
+
+const max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 20)
+
+
+
+
+
+
+
+  
+
+ 
+
+
+
+
+// function multiply(num1, num2) {
+
+//      return num1 * num2; 
+//  }
+
+//  multiply(31, 2)
